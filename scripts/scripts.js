3347f6e3be1a33b8a17937c3b20946643f638e02
Abhishek Thakur <abhthakur@adobe.com>
2026-07-27 12:38:57 +0530

    Broken new changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,17 +14,7 @@ import {
 
 
 
-/**
- * load fonts.css and set a session storage flag
- */
-async function loadFonts() {
-  await loadCSS(`${window.hlx.codeBasePath}/styles/fonts.css`);
-  try {
-    if (!window.location.hostname.includes('localhost')) sessionStorage.setItem('fonts-loaded', 'true');
-  } catch (e) {
-    // do nothing
-  }
-}
+
 
 /**
  * Turns `/widgets/...` links into widget blocks.
